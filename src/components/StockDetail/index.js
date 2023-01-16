56ac587bd999d34afe0b1207b544fda8c8a85fb9
Winston <wlnston1@gmail.com>
2023-01-16 11:27:11 -0800

    added working detail rendering

--- src/components/StockDetail/index.js
+++ src/components/StockDetail/index.js
@@ -1,11 +1,24 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import data from '../../data.json';
 
 function StockDetail(props)
 {
+    const {id} = useParams();
+    console.log(data.stocks);
+
     return (
         <div>
-            <h3> stock details </h3>
+            <h2> detail </h2>
+            <h4> {data.stocks[2].symbol} </h4>
+            <h4> {id} </h4>
+            <div>
+            {data.stocks.map(stock => {
+                if (stock.symbol === id)
+                    return <h2> {stock.name} </h2>
+                // why if statement not render
+            })}
+            </div>
         </div>
     )
 }
